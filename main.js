2152713eef24c962d9e27a462996b8923483758d
RichSpring39 <richard.springer@codeimmersives.com>
2022-09-19 16:22:31 -0600

    finished

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-let choice = prompt("combine or deconstruct:")
+let choice = prompt("combine or deconstruct: ")
 if(choice === "combine"){
-let color1 = prompt("First color");
-let color2 = prompt("second color ");
+let color1 = prompt("First color: ");
+let color2 = prompt("second color: ");
 
 if (color2 === "red" && color1 === "blue"){
     console.log("purple");
@@ -22,7 +22,7 @@ if (color2 === "red" && color1 === "blue"){
 } 
 }
 if (choice === "deconstruct"){ 
-    let color = prompt("color");
+    let color = prompt("color: ");
 
 if (color === "purple") {
     console.log("red + blue")
